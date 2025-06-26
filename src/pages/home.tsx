763da58efa9ad86f8eaa3d7a48454e5d3cc6b0e5
mrmihi <mrmihiraj@gmail.com>
2025-06-26 20:31:05 +0530

    chore: style changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,9 +4,9 @@ import ResultDialog from '@/components/result-dialog';
 export function Home() {
     return (
         <>
-            <div className="flex w-full h-full flex-1 flex-col justify-center items-center p-8 md:p-10 relative">
+            <div className="flex w-full h-full flex-1 flex-col justify-center items-center p-8 md:p-5 relative">
                 <div className="absolute my-0 transform translate-y-11 w-full h-1 bg-gradient-to-r from-transparent via-white/50 to-transparent blur-lg" />
-                <div className="w-full z-10 justify-center items-center flex flex-col gap-8">
+                <div className="w-full max-w-4xl mx-auto z-10 justify-center items-center flex flex-col gap-8">
                     <HomeForm />
                 </div>
             </div>
